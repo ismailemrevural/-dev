42d7760e7b10c93c71556a0d36bb22907c6e3417
ismail emre vural <121684249+ismailemrevural@users.noreply.github.com>
2023-03-12 19:35:00 +0300

    Add files via upload
    Ogrenci Bilgileri Struct Kodu

--- main.c
+++ main.c
@@ -2,34 +2,13 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
- void  kucukbulma(int array[])
-{
-	int enkucuk=array[0];	
-	int i;															
-	int n;
-	for(i=0;i<n;i++)
-	{
-		if(enkucuk>array[i])
-		{
-		enkucuk=array[i];	
-		}
-	}
-	 printf("En kucuk eleman %d",enkucuk);
-
-}
 
 int main() {
-	int x;
-	printf("Dizinin eleman Sayisini Giriniz\n");
-	scanf("%d",&x);
-		int  dizi[x];
-	int i;
-	printf("Dizinin Elemanlrini Giriniz\n");
-	for(i=0;i<x;i++)
-	{
-		scanf("%d",&dizi[i]);
-	}
-		kucukbulma(dizi);
+	
+char tcno[20]=45673219052;
+printf("%s",tcno);	
+	
+	
 
 	return 0;
 }
